804d0a927b5964029537e2d8ebabaec834be1bf3
Rinnegatamante <rinnegatamante@gmail.com>
2020-03-24 14:53:47 +0100

    Fix for console input not properly showing sometimes.

--- source/console.c
+++ source/console.c
@@ -491,7 +491,6 @@ The input line scrolls horizontally if typing goes beyond the right edge
 */
 void Con_DrawInput (void)
 {
-	int		y;
 	int		i;
 	char	*text;
 
@@ -512,10 +511,8 @@ void Con_DrawInput (void)
 		text += 1 + key_linepos - con_linewidth;
 
 // draw it
-	y = con_vislines-16;
-
 	for (i=0 ; i<con_linewidth ; i++)
-		Draw_Character ( (i+1)<<3, con_vislines - 16, text[i]);
+		Draw_Character ( (i+1)<<3, vid.conheight - 16, text[i]);
 
 // remove cursor
 	key_lines[edit_line][key_linepos] = 0;
